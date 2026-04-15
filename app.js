927c47f040372d0e82550fb29b5493ee5a420aeb
EC2 Default User <ec2-user@ip-172-31-31-31.eu-central-1.compute.internal>
2026-04-15 22:48:11 +0000

    Initial ShipFasts app

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express'); 
+const app = express(); 
+app.get('/', (req, res) => { 
+  res.send('ShipFast App is running ✅'); 
+}); 
+app.get('/health', (req, res) => { 
+  res.status(200).send('OK'); 
+}); 
+app.listen(3060, () => { 
+  console.log('App running on port 3060'); 
+}); 
